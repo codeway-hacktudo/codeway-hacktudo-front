4a97de0f0bcc5145d8ecf06b45ae0485dcd172f3
celsoernani <celsoernane@gmail.com>
2021-10-16 03:53:36 -0300

    folder structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return <div>preciso de token</div>
+}
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+ * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+   outline: 0;
+
+ }
+ body {
+   background: #FFF;
+   color: #303f42;
+   -webkit-font-smoothing: antialiased;
+ }
+
+ body, input, button {
+   font-family: 'Roboto Slab', serif;
+   font-size: 16px;
+   input:-webkit-autofill {
+    color: #303f42, !important;
+}
+ }
+
+ h1,h2,h3,h4,h5,h5, strong {
+   font-weight: 500;
+ }
+
+ button {
+   cursor: pointer;
+ }
+`;
